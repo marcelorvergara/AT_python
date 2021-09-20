6f0e39c28be8b94ce8b98a441c54411899a9a263
Marcelo Vergara <marcelorv@gmail.com>
2021-09-20 13:26:27 -0300

    correção q. 5

--- Questao5.py
+++ Questao5.py
@@ -7,12 +7,12 @@ class Questao5(threading.Thread):
         super().__init__()
         print("Entre com cinco números inteiros:")
         tupla_nums = (3, 6, 2, 7, 8, 3, 4, 8, 9, 8, 3, 1)
-        pares = []
-        impares = ()
+        pares = ()
+        impares =[]
         for n in tupla_nums:
-            if n % 2 == 0:
-                pares.append(n)
-            else:
-                impares = impares + (n,)
-        print("Lista de pares", pares)
-        print("Tupla de ímpares", impares)
+            if tupla_nums.index(n) % 2 == 0:
+                pares = pares + (n,)
+            if n % 2 != 0:
+                impares.append(n)
+        print("Tupla das posições pares", pares)
+        print("Lista de ímpares", impares)
